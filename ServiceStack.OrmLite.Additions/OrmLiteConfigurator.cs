c565f1f8446feff4b3062883d00813dedf38bac2
Jarrod Alexander <jarroda@2020research.com>
2014-04-03 09:16:19 -0500

    Initial commit

---
+++ ServiceStack.OrmLite.Additions/OrmLiteConfigurator.cs
@@ -0,0 +1,88 @@
+﻿using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Text;
+
+namespace OrmLiteEx
+{
+    public static class OrmLiteConfigurator
+    {
+        public static Dictionary<Type, ModelDefinition> GetConfigMap()
+        {
+            var fieldInfo = (FieldInfo)typeof(OrmLiteConfig).Assembly.GetType("ServiceStack.OrmLite.OrmLiteConfigExtensions")
+                .GetMember("typeModelDefinitionMap", MemberTypes.Field, BindingFlags.Static | BindingFlags.NonPublic).First();
+
+            return (Dictionary<Type, ModelDefinition>)fieldInfo.GetValue(null);
+        }
+
+        public class ModelDefinition<T> : ModelDefinition { }
+
+        public static ModelDefinition<T> AddModelDefinition<T>(this Dictionary<Type, ModelDefinition> dict, string alias = null)
+        {
+            var modelType = typeof(T);
+            var def = new ModelDefinition<T>
+            {
+                ModelType = modelType,
+                Name = modelType.Name,
+                Alias = alias ?? modelType.Name,
+            };
+            dict.Add(modelType, def);
+            return def;
+        }
+
+        public static ModelDefinition<T> AddField<T>(this ModelDefinition<T> modelDef, Expression<Func<T, object>> exp, bool isNullable = false, bool isPrimaryKey = false, bool isAutoIncrement = false,
+            string alias = null, string defaultValue = null, Type references = null, string onDelete = null, string onUpdate = null)
+        {
+            var propInfo = modelDef.ModelType.GetPropertyInfo(exp.Name());
+
+            modelDef.FieldDefinitions.Add(new FieldDefinition
+            {
+                Name = alias ?? propInfo.Name,
+                FieldType = propInfo.PropertyType,
+                PropertyInfo = propInfo,
+                GetValueFn = propInfo.GetPropertyGetterFn(),
+                SetValueFn = propInfo.GetPropertySetterFn(),
+                IsNullable = isNullable,
+                IsPrimaryKey = isPrimaryKey,
+                AutoIncrement = isAutoIncrement,
+                ForeignKey = references == null ? null : new ForeignKeyConstraint(references, onDelete, onUpdate),
+                DefaultValue = defaultValue,
+            });
+
+            return modelDef;
+        }
+
+        public static ModelDefinition<T> AddIgnoredField<T>(this ModelDefinition<T> modelDef, Expression<Func<T, object>> exp)
+        {
+            var propInfo = modelDef.ModelType.GetPropertyInfo(exp.Name());
+
+            modelDef.IgnoredFieldDefinitions.Add(new FieldDefinition
+            {
+                Name = propInfo.Name,
+                FieldType = propInfo.PropertyType,
+                PropertyInfo = propInfo,
+                GetValueFn = propInfo.GetPropertyGetterFn(),
+                SetValueFn = propInfo.GetPropertySetterFn(),
+            });
+
+            return modelDef;
+        }
+
+        public static string Name<T>(this Expression<Func<T, object>> exp)
+        {
+            MemberExpression body = exp.Body as MemberExpression;
+            if (body == null)
+            {
+                UnaryExpression ubody = (UnaryExpression)exp.Body;
+                body = ubody.Operand as MemberExpression;
+            }
+
+            return body.Member.Name;
+        }
+    }
+}
---
+++ ServiceStack.OrmLite.Additions/ReliableSqlServerOrmLiteDialectProvider.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.OrmLite;
+using ServiceStack.OrmLite.SqlServer;
+using System.IO;
+using System.Data;
+
+namespace OrmLiteEx
+{
+    public class ReliableSqlServerOrmLiteDialectProvider : SqlServerOrmLiteDialectProvider
+    {
+        public ReliableSqlServerOrmLiteDialectProvider() : base()
+        {this.
+            RetryPolicy = RetryPolicy.DefaultExponential;
+        }
+
+        public RetryPolicy RetryPolicy { get; set; }
+
+        public override IDbConnection CreateConnection(string connectionString, Dictionary<string, string> options)
+        {
+            var isFullConnectionString = connectionString.Contains(";");
+
+            if (!isFullConnectionString)
+            {
+                var filePath = connectionString;
+
+                var filePathWithExt = filePath.ToLower().EndsWith(".mdf")
+                    ? filePath
+                    : filePath + ".mdf";
+
+                var fileName = Path.GetFileName(filePathWithExt);
+                var dbName = fileName.Substring(0, fileName.Length - ".mdf".Length);
+
+                connectionString = string.Format(
+                @"Data Source=.\SQLEXPRESS;AttachDbFilename={0};Initial Catalog={1};Integrated Security=True;User Instance=True;",
+                    filePathWithExt, dbName);
+            }
+
+            if (options != null)
+            {
+                foreach (var option in options)
+                {
+                    if (option.Key.ToLower() == "read only")
+                    {
+                        if (option.Value.ToLower() == "true")
+                        {
+                            connectionString += "Mode = Read Only;";
+                        }
+                        continue;
+                    }
+                    connectionString += option.Key + "=" + option.Value + ";";
+                }
+            }
+
+            return new ReliableSqlConnection(connectionString, RetryPolicy);
+        }
+    }
+}
